1154601bd068309f2877eaa560718967a4ce6149
Jéssica Pedro <jessicapedro165@gmail.com>
2022-02-07 17:45:07 +0000

    atualização

---
+++ menu_interno.js
@@ -0,0 +1,15 @@
+/* function troca_menu(){
+    let amazon = document.getElementById("amazon");
+    let google = document.getElementById("google");
+    
+    if()
+} 
+
+( function (){
+    let amazon = document.getElementById("amazon");
+    let google = document.getElementById("google");
+    
+    if(amazon.style.display = "block"){
+        google.style.display = "none";
+    } else 
+})*/
