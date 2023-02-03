de6217b8c78069a2630f728fb6a756bcd76128cf
Jéssica Pedro <jessicapedro165@gmail.com>
2023-02-03 10:33:10 +0000

    update

--- js/light_mode.js
+++ js/light_mode.js
@@ -1,8 +1,8 @@
 var mode = document.getElementById("mode");
 
 mode.onclick = function (){
-    document.body.classList.toggle("light-mode");
-    if(document.body.classList.contains("light-mode")){
+    document.body.classList.toggle("dark-mode");
+    if(document.body.classList.contains("dark-mode")){
         mode.src = "img/dark theme icon/sun.png";
     } else {
         mode.src = "img/dark theme icon/moon.png";
